11edaf875c564b0f5c23e2f41d199ac430dab1aa
Bram Suurd <contact@bramsuurd.nl>
2024-11-16 00:08:11 +0100

    Fix formatting in src/app/page.tsx to conform with ESLint rules and maintain code consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export default function page() {
-  return (
-    <div>
-      Hello from NHL Stenden 👋
-    </div>
-  )
+  return <div>Hello from NHL Stenden 👋</div>;
 }
