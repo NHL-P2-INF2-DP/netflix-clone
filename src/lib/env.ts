93ab7d2a18d61c25f7977e1a9c14b1304cfd1d7b
Bram Suurd <contact@bramsuurd.nl>
2024-11-25 19:21:24 +0100

    feat: add DatabaseService class for handling database operations and update env variables for PostgreSQL connection

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -13,21 +13,16 @@ expand(
   }),
 );
 
-const EnvSchema = z.object({
+const envSchema = z.object({
   // add new environment variables here, this will allow you to access them in your app in a type-safe manner
   NODE_ENV: z.string().default('development'),
   PORT: z.coerce.number().default(3000),
+  JWT_SECRET: z.string().min(32),
+  DATABASE_URL: z.string(),
 });
 
-export type env = z.infer<typeof EnvSchema>;
-
-// eslint-disable-next-line ts/no-redeclare
-const { data: env, error } = EnvSchema.safeParse(process.env);
-
-if (error) {
-  console.error('❌ Invalid env:');
-  console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
-  process.exit(1);
+declare module 'node' {
+  interface ProcessEnv extends z.infer<typeof envSchema> {}
 }
 
-export default env!;
+export const env = envSchema.parse(process.env);
