0007a63437d7fdf3b55da3fa8be89240825dbfb2
Bram Suurd <contact@bramsuurd.nl>
2024-12-15 23:57:29 +0100

    chore: update health route to use http-status-codes for cleaner status management

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,11 +1,13 @@
 import type { NextRequest } from 'next/server';
 
+import { StatusCodes } from 'http-status-codes';
+
 import { ResponseFormatter } from '@/lib/classes/api-responses';
 
 export function GET(request: NextRequest) {
   return ResponseFormatter.formatResponse(
     { message: 'OK' },
     request.headers.get('Accept') || 'application/json',
-    200,
+    StatusCodes.OK,
   );
 }
