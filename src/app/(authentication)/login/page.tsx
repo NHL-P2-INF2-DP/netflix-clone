65b6db36102c2c1615a4de1a93ec8ecf8d3d59ba
Bram Suurd <contact@bramsuurd.nl>
2024-12-16 18:45:47 +0100

    feat: update login page to use react-hook-form for form handling and validation

--- src/app/(authentication)/login/page.tsx
+++ src/app/(authentication)/login/page.tsx
@@ -1,30 +1,43 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { toast } from 'sonner';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { authClient } from '@/lib/auth-client';
+import { cn } from '@/lib/utils';
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const router = useRouter();
-  const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(
+      z.object({
+        email: z.string().email('Invalid email address'),
+        password: z.string().min(1, 'Password is required'),
+      }),
+    ),
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    setError('');
     setIsLoading(true);
     authClient.signIn.email(
       {
-        email,
-        password,
+        email: data.email,
+        password: data.password,
       },
       {
         onSuccess: (data) => {
@@ -32,69 +45,82 @@ export default function LoginPage() {
           setIsLoading(false);
         },
         onError: (ctx) => {
-          toast.error(ctx.error.message);
-          setIsLoading(false);
+          // set a delay of 3 seconds before showing the error to the user
+          setTimeout(() => {
+            setError(ctx.error.message);
+            setIsLoading(false);
+          }, 3000);
         },
       },
     );
-  };
+  });
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-black bg-opacity-75 bg-[url('/placeholder.svg?height=1080&width=1920')] bg-cover bg-center bg-blend-darken">
-      <Card className="w-full max-w-md border-none bg-black/80 text-white">
+      <Card className="w-full max-w-md rounded-2xl border-none bg-black/80 text-white">
         <CardHeader>
           <CardTitle className="text-center text-3xl font-bold text-red-600">
             NETFLIX DBMS
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={onSubmit}>
             <div className="space-y-4">
               <div className="space-y-2">
                 <Label htmlFor="email" className="text-gray-400">
-                  Email or phone number
+                  Email
                 </Label>
                 <Input
                   id="email"
-                  type="email"
+                  disabled={isLoading}
+                  type="text"
+                  {...register('email')}
                   required
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
                   className="border-gray-600 bg-gray-700 text-white placeholder-gray-400 focus-visible:ring-red-600"
                 />
+                {errors.email && (
+                  <p className="mt-2 text-sm text-red-500">
+                    {errors.email.message as string}
+                  </p>
+                )}
               </div>
               <div className="space-y-2">
-                <Label htmlFor="password" className="text-gray-400">
+                <Label
+                  htmlFor="password"
+                  className={cn(
+                    'text-gray-400',
+                    errors.password && 'text-red-600',
+                  )}
+                >
                   Password
                 </Label>
                 <Input
                   id="password"
+                  disabled={isLoading}
                   type="password"
+                  {...register('password')}
                   required
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  className="border-gray-600 bg-gray-700 text-white placeholder-gray-400 focus-visible:ring-red-600"
+                  className={cn(
+                    'border-gray-600 bg-gray-700 text-white placeholder-gray-400 focus-visible:ring-red-600',
+                    errors.password && 'border-red-600',
+                  )}
                 />
+                {errors.password && (
+                  <p className="mt-2 text-sm text-red-500">
+                    {errors.password.message as string}
+                  </p>
+                )}
               </div>
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                  id="remember"
-                  className="border-gray-600 data-[state=checked]:border-red-600 data-[state=checked]:bg-red-600"
-                />
-                <Label htmlFor="remember" className="text-sm text-gray-400">
-                  Remember me
-                </Label>
-              </div>
+              {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
             </div>
             <Button
               type="submit"
               disabled={isLoading}
               className="mt-6 w-full bg-red-600 text-white hover:bg-red-700"
             >
-              Sign In
+              {isLoading ? 'Loading...' : 'Sign In'}
             </Button>
           </form>
-          {error && <p className="mt-2 text-sm text-red-500">{error}</p>}
         </CardContent>
       </Card>
     </div>
