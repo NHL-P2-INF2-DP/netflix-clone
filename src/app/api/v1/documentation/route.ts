e48ceffd8f019bc6365ce8e5a1947ba7e45cf89f
Bram Suurd <contact@bramsuurd.nl>
2024-12-17 22:08:27 +0100

    feat: enhance OpenAPI configuration by adding query parameters for relation inclusion and updating hidden clients list

--- src/app/api/v1/documentation/route.ts
+++ src/app/api/v1/documentation/route.ts
@@ -4,10 +4,61 @@ const config = {
   spec: {
     url: '/api/v1/openapi',
   },
-  defaultHttpClient: {
-    targetKey: 'javascript',
-    clientKey: 'fetch',
+  config: {
+    parameters: {
+      global: [
+        {
+          name: 'extend',
+          in: 'query',
+          description:
+            'Comma-separated list of relations to include in the response',
+          required: false,
+          schema: {
+            type: 'string',
+            example: 'relation1,relation2',
+          },
+        },
+      ],
+    },
   },
+  hiddenClients: [
+    'libcurl',
+    'clj_http',
+    'httpclient',
+    'restsharp',
+    'native',
+    'http1.1',
+    'asynchttp',
+    'nethttp',
+    'okhttp',
+    'unirest',
+    'xhr',
+    'axios',
+    'ofetch',
+    'jquery',
+    'okhttp',
+    'native',
+    'request',
+    'unirest',
+    'axios',
+    'nsurlsession',
+    'cohttp',
+    'curl',
+    'guzzle',
+    'http1',
+    'http2',
+    'webrequest',
+    'restmethod',
+    'python3',
+    'requests',
+    'httr',
+    'native',
+    'httpie',
+    'wget',
+    'nsurlsession',
+    'undici',
+  ],
+  hideClientButton: true,
 };
 
 export const GET = ApiReference(config);
